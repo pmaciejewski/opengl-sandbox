15bd22af70caf3d1413cc45af2372e9938ff3464
Pawe <pawel@ubuntu.ubuntu-domain>
2010-04-21 21:43:31 +0200

    cos zaczyna dzialac :)

--- hello.c
+++ hello.c
@@ -1,10 +1,25 @@
 #include <GL/glut.h>
 #include<stdio.h>
 
+static float b, c;
+
 void display(void)
 {
 /* clear all pixels  */
    glClear (GL_COLOR_BUFFER_BIT);
+   float b1,c1; //statica chyba nie mozna modyfikowac; 
+   float ya,yb;
+   float xa=0.00,xb=1; //argumenty - trzeba policzyć dla malej wartosci, bo inaczej wyjdzie z zakresu 0.0-1.0 i wtedy nie rysuje wykresu
+   
+   b1 = b/100;
+   c1 = c/100;
+   /*
+   obliczamy dwa punkty nalezace do wykresu funkcji liniowej
+   */
+   ya = b1*xa+c1;
+   yb = b1*xb+c1;
+   
+   
    
  /* 
  cos na ksztalt ukladu wspolrzednych 
@@ -27,8 +42,8 @@ void display(void)
   /* wykres funkcji */
   glColor3f(0.55,1.0,1.0);
   glBegin(GL_LINES);
-    glVertex3f(1,0.0,0.0); 
-    glVertex3f(0.0,1.0,0.0);
+    glVertex3f(xa,ya,0.0); 
+    glVertex3f(xb,yb,0.0);
   glEnd();
   
   
@@ -63,6 +78,10 @@ void init (void)
 	    
 int main(int argc, char** argv)
 {
+   
+   scanf ("%f", &b);
+   scanf ("%f", &c);
+   
    
    glutInit(&argc, argv);
    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
